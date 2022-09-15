b7cd54ffd5696071d73764d364338425d8a148c6
Arthur Papp <59942836+arthurPapp@users.noreply.github.com>
2022-09-14 23:45:48 -0300

    servidor

---
+++ src/server.ts
@@ -0,0 +1,45 @@
+import express, { response } from 'express';
+
+//criando a api em si
+const app = express();
+
+// criando a 1 rota
+//no parametro get() 1 parametro se passa o endereço que o usuario vai acessar após a barra do host ex https://www.minhaapi.com/ads
+// no 2 parametro passamos a função que o user vai acessar
+//a function recebe 2 parametreos request e response, ela precisa devolver algo
+// request -> busco dados vindo da requisição
+//response -> responde a requisição
+//dentro do json posso usar qualquer tipo primitivo
+app.get('/ads', (request, response) => {
+    // return response.send('Acessou ads!')
+    return response.json([
+        { id: 1, name: 'anuncio1' },
+        { id: 2, name: 'anuncio2' },
+        { id: 3, name: 'anuncio3' },
+        { id: 4, name: 'anuncio4' }
+    ])
+});
+
+//passamos a porta para o node diferenciar a porta
+//aqui tbm indico que é para a api ficar excultando ate eu pedir que ela pare 
+
+app.listen(8888)
+
+
+//estudar TypeScript para adicionar ao js(TypeScript e uma linguagem de tipagem estatica)
+//vamos usar o typeScript nesse projeto (npm i typescript)
+//o node não entende typescript nativamente por isso usamos só como ferramenta de desenvolvimento
+//mudo o nome do arquivo para .ts
+// ao usar o TypeScript pracisamos usar um arquivo de configuracao para criar esse arquivo usamos o camando npx tsc --init que ira execultar o arquivo tsc
+// no arquivo tsconfig.json trocar o module:commonjs por ES2020
+
+
+//tsconfig descomentar rootDir e auterar para a pasta onde estão os arquivos de inicio "rootDir": "./src",  e outDir onde ira criar os arquivos de build  "outDir": "./build",
+
+
+//como o express não e nativo typeScript e preciso instalar o @types/express (sempre verificar na net)
+
+
+// sempre q eu att o arquivo vai ser preciso reiniciar o node, pra isso instalamos ts-node-dev -D
+
+//no arquivo tsconfig.json voltar o module:commonjs para usar o tsnd src/server.ts
