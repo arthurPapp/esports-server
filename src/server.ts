f5383188ebf0525776be90b1969b701ee3514fce
Arthur Papp <59942836+arthurPapp@users.noreply.github.com>
2022-09-15 20:01:25 -0300

    backend sem validações

--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,99 @@
 import express, { response } from 'express';
+import { PrismaClient } from '@prisma/client'
+import { convertHourStringToMinutes } from './utils/convert-hour-to-minutes';
+import { convertMinutesToHoursString } from './utils/convert-minutes-to-hours-string';
+import cors from 'cors';
 
-//criando a api em si
 const app = express();
 
-// criando a 1 rota
-//no parametro get() 1 parametro se passa o endereço que o usuario vai acessar após a barra do host ex https://www.minhaapi.com/ads
-// no 2 parametro passamos a função que o user vai acessar
-//a function recebe 2 parametreos request e response, ela precisa devolver algo
-// request -> busco dados vindo da requisição
-//response -> responde a requisição
-//dentro do json posso usar qualquer tipo primitivo
-app.get('/ads', (request, response) => {
-    // return response.send('Acessou ads!')
-    return response.json([
-        { id: 1, name: 'anuncio1' },
-        { id: 2, name: 'anuncio2' },
-        { id: 3, name: 'anuncio3' },
-        { id: 4, name: 'anuncio4' }
-    ])
+//informando ao express que vamos usar o formato json
+app.use(express.json());
+//depois passar o host do front para só ele conectar
+app.use(cors());
+
+// dentro do prisma possa passa parametro de conf como o log
+const prisma = new PrismaClient({
+    log:['query']
 });
 
-//passamos a porta para o node diferenciar a porta
-//aqui tbm indico que é para a api ficar excultando ate eu pedir que ela pare 
-
-app.listen(8888)
-
-
-//estudar TypeScript para adicionar ao js(TypeScript e uma linguagem de tipagem estatica)
-//vamos usar o typeScript nesse projeto (npm i typescript)
-//o node não entende typescript nativamente por isso usamos só como ferramenta de desenvolvimento
-//mudo o nome do arquivo para .ts
-// ao usar o TypeScript pracisamos usar um arquivo de configuracao para criar esse arquivo usamos o camando npx tsc --init que ira execultar o arquivo tsc
-// no arquivo tsconfig.json trocar o module:commonjs por ES2020
-
-
-//tsconfig descomentar rootDir e auterar para a pasta onde estão os arquivos de inicio "rootDir": "./src",  e outDir onde ira criar os arquivos de build  "outDir": "./build",
+app.get('/games', async (request, response) => {
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads:true
+                }
+            }
+        }
+    });
+    return response.json(games); 
+});
 
 
-//como o express não e nativo typeScript e preciso instalar o @types/express (sempre verificar na net)
+app.get('/games/:id/ads', async(request, response) => {
+    const gameId = request.params.id;
+    const ads = await prisma.ad.findMany({
+        select: {
+            id: true,
+            name: true,
+            yearsPlaying: true,       
+            weekDays: true,
+            hourStart: true,
+            hourEnd: true,
+            useVoiceChannel: true,           
+        },
+        where: {
+            gameId:gameId,
+        },
+        orderBy:{
+            createdAt: 'desc',
+        }
+    })
+    return response.json(ads.map(ad => {
+        return {    
+            //retorna os dados da msma forma mas eu faço um spredOperator no objeto e substituo uma info nesse objeto
+            ...ad,
+            weekDays: ad.weekDays.split(','),
+            hourStart: convertMinutesToHoursString(ad.hourStart),
+            hourEnd: convertMinutesToHoursString(ad.hourEnd),
+        }
+    }));
+});
 
+app.get('/ads/:id/discord', async (request, response) => {
+    const adId = request.params.id;
+    const ad = await prisma.ad.findUniqueOrThrow({
+        select: {
+           discord:true 
+        },
+        where: {
+            id: adId,
+        }
+    });
+    return response.json({
+        discord: ad.discord,
+    })
+});
 
-// sempre q eu att o arquivo vai ser preciso reiniciar o node, pra isso instalamos ts-node-dev -D
+app.post('/games/:id/ads', async (request, response) => {
+    const gameId = request.params.id;
+    const body: any = request.body;
+    
+    //Validações
+    const ad = await prisma.ad.create({
+        data: {
+            gameId,
+            name:body.name,
+            yearsPlaying:body.yearsPlaying,
+            discord:body.discord,
+            weekDays:body.weekDays.join(','),
+            hourStart:convertHourStringToMinutes(body.hourStart),
+            hourEnd:convertHourStringToMinutes(body.hourEnd),
+            useVoiceChannel:body.useVoiceChannel
+        }
+    })
 
-//no arquivo tsconfig.json voltar o module:commonjs para usar o tsnd src/server.ts
+    return response.status(201).json([ad]); 
+});
+
+app.listen(8888);
